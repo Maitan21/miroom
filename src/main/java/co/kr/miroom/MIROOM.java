1ad8e47c584bcbafb83d26cddcf01829938ed964
Maitan21 <oklfg2002@gmail.com>
2020-10-10 18:12:12 +0900

    init project

---
+++ src/main/java/co/kr/miroom/MIROOM.java
@@ -0,0 +1,13 @@
+package co.kr.miroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MIROOM {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MIROOM.class, args);
+    }
+
+}
