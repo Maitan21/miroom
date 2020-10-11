af2027da9648179551ed56f63405eb9f50201f01
Maitan21 <oklfg2002@gmail.com>
2020-10-11 13:22:35 +0900

    Add login Handler and tos

--- src/main/java/co/kr/miroom/MIROOM.java
+++ src/main/java/co/kr/miroom/MIROOM.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MIROOM {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MIROOM.class, args);
     }
 
